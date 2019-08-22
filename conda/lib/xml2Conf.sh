c1dfd7813b840cd3ea3e5846c57512872ed40df4
yan30 <yan30@iu.edu>
2019-08-22 19:57:12 +0000

    conda loaded

---
+++ conda/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/jovyan/ISSItutorial/conda/lib"
+XML2_LIBS="-lxml2 -L/home/jovyan/ISSItutorial/conda/lib -lz -L/home/jovyan/ISSItutorial/conda/lib -llzma -lpthread -L/home/jovyan/ISSItutorial/conda/lib -liconv -L/home/jovyan/ISSItutorial/conda/lib -licui18n -licuuc -licudata -lm "
+XML2_INCLUDEDIR="-I/home/jovyan/ISSItutorial/conda/include/libxml2 -I/home/jovyan/ISSItutorial/conda/include"
+MODULE_VERSION="xml2-2.9.9"
+
